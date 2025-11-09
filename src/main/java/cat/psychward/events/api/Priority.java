5a18ef1f3b3147bb14c616e31b44077348af6d40
aurelia <synthetic@psychward.cat>
2025-11-09 12:56:31 +0100

    fix: flip priority order

--- src/main/java/cat/psychward/events/api/Priority.java
+++ src/main/java/cat/psychward/events/api/Priority.java
@@ -8,10 +8,10 @@ public final class Priority {
 
     private Priority() {}
 
-    public static final int LOWEST = -2;
-    public static final int LOW = -1;
+    public static final int HIGHEST = -2;
+    public static final int HIGH = -1;
     public static final int NORMAL = 0;
-    public static final int HIGH = 1;
-    public static final int HIGHEST = 2;
+    public static final int LOW = 1;
+    public static final int LOWEST = 2;
 
 }
